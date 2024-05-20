2867339abe9b4e24a672854ad045171270e50007
satya <satyabathula453@gmail.com>
2024-05-20 07:08:11 +0530

    Initial commit

---
+++ src/components/CreateEntity.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const CreateEntity = () => {
+  // Component logic for creating entities
+  return (
+    <div>
+      <h2>Create Entity</h2>
+      {/* Form for creating entities */}
+    </div>
+  );
+};
+
+export default CreateEntity;
---
+++ src/components/EntityCRUD.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const EntityCRUD = () => {
+  // Component logic for CRUD operations on entities
+  return (
+    <div>
+      <h2>Entity CRUD</h2>
+      {/* Display list of entities and CRUD operations */}
+    </div>
+  );
+};
+
+export default EntityCRUD;
